9989f3ccc5b361d3848975e5b9f9a3ae3bec3d1c
qualityspa-configurateur <louiscanillos@gmail.com>
2026-06-10 20:35:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let coqueActuelle = "blanc-nuage";
+let tablierActuel = "bois-teck";
+
+function changerCoque(coque, boutonClique) {
+    coqueActuelle = coque;
+
+    document.querySelectorAll(".coque-btn").forEach(button => {
+        button.classList.remove("actif");
+    });
+
+    boutonClique.classList.add("actif");
+
+    mettreAJourConfigurateur();
+}
+
+function changerTablier(tablier, boutonClique) {
+    tablierActuel = tablier;
+
+    document.querySelectorAll(".tablier-btn").forEach(button => {
+        button.classList.remove("actif");
+    });
+
+    boutonClique.classList.add("actif");
+
+    mettreAJourConfigurateur();
+}
+
+function mettreAJourConfigurateur() {
+    const image = document.getElementById("spa-image");
+    const nouvelleImage = "images/lagos-" + coqueActuelle + "-" + tablierActuel + ".png";
+
+    image.classList.add("fade");
+
+    setTimeout(() => {
+        image.src = nouvelleImage;
+        image.classList.remove("fade");
+    }, 200);
+}
+
+function telechargerVisuel() {
+    const cheminImage = "images/lagos-" + coqueActuelle + "-" + tablierActuel + ".png";
+
+    fetch(cheminImage)
+        .then(response => response.blob())
+        .then(blob => {
+            const url = URL.createObjectURL(blob);
+            const lien = document.createElement("a");
+
+            lien.href = url;
+            lien.download = "lagos-" + coqueActuelle + "-" + tablierActuel + ".png";
+
+            document.body.appendChild(lien);
+            lien.click();
+            document.body.removeChild(lien);
+
+            URL.revokeObjectURL(url);
+        });
+}
